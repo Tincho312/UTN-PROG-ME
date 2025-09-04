fd30946833e890f208a7b13ebdc1237f4eaf3388
MartinDeveloperWeb <martinescudero256@gmail.com>
2025-09-04 08:29:56 -0300

    Actualizacion2

--- PROGRAMACION I/02 Trabajo Colaborativo/Ejercicio1.py
+++
@@ -1,9 +0,0 @@
-# 1- CASTEO: Codifique un programa que solicite el ingreso de un número decimal y 
-# asigne el mismo a una variable valorDecimal, aplique las operaciones de CASTING para 
-# convertir la variable en otro tipo de dato. Investigue las diferentes formas de lograr la 
-# conversión. Muestre por pantalla el resultado de las conversiones.  
-
-valorDecimal = float(input("ingrese un numero Decimal "))
-numCasteado = int(valorDecimal)
-print("Numero Ingresado=",valorDecimal,"Numero Casteado a entero=",numCasteado)
-
--- PROGRAMACION I/02 Trabajo Colaborativo/Ejercicio2.py
+++
@@ -1,8 +0,0 @@
-# 2- Si se asigna un valor a una variable fuera de rango (mayor de lo establecido) ¿Qué 
-# ocurre? ¿Existe alguna forma de resolverlo? Ejemplifique.  
-
-numero = int(input("Ingrese un número entre 1 y 10: "))
-if 1 <= numero <= 10:
-    print("Número válido:", numero)
-else:
-    print("Error: número fuera de rango.")
